56067773be550a41826f1de7c7d23dfe2e077aaa
LazyMan95 <nico95.nm@gmail.com>
2024-04-22 13:18:56 +0200

    Update the project

--- app/src/main/java/game/App.java
+++ app/src/main/java/game/App.java
@@ -7,9 +7,10 @@ import java.util.Scanner;
 
 public class App {
     Scanner scan = new Scanner(System.in);
+    Scanner scanOne = new Scanner(System.in);
     String playerName;
     String playerWeapon;
-    String potion = "";
+    String potion;
     int playerHP;
     int goblinHP;
     int choice;
@@ -33,13 +34,13 @@ public class App {
 
     public void playerInfo() {
         // ----------------
-        playerHP = 50;
+        playerHP = 25;
         playerCoin = 10;
         playerWeapon = "Knife";
         innkeeperCoin = 10;
         potion = "";
+        goblinHP = 20;
         // ----------------
-
         // ----------------
         System.out.println("\n----------------------------------------------------------------");
         System.out.println("Welcome adventurer to this world please choose a name: ");
@@ -141,12 +142,21 @@ public class App {
         System.out.println("");
         System.out.println(
                 "Guard: I see if that is the case the rumors are true the king lost his Legendary Ring while he was hunting. Some scouts have seen goblin wearing the ring on the neck, king have send some warriors to retrieve it but non returned. I wish you luck retrieving it, you will need it. But go to the Crossway not far from here, and from there and you wanna go to the east, its a big forest\n");
-        System.out.println("1. Go to the crossroad");
+        System.out.println("1. Go to the Crossroad");
+        System.out.println("2. Go to the Castle");
+        System.out.println("");
         choice = scan.nextInt();
         if (choice == 1) {
             crossRoad();
-        } else {
-            townGate();
+        } else if (choice == 2) {
+            if (legendaryRing == 1) {
+
+                end();
+            } else {
+                System.out.println("You dont have a ring!");
+                System.out.println("\n----------------------------------------------------------------");
+                townGate();
+            }
         }
     }
 
@@ -185,6 +195,7 @@ public class App {
      * knowing.[-]
      */
     public void north() {
+        // River location
         System.out.println("Location: North");
         System.out.println("You see the river and you wanted to have a rest and you feel thirsty.");
         System.out.println(
@@ -205,38 +216,170 @@ public class App {
             north();
         }
     }
+    // -------------------------------------
 
-    /* Combat I will do the last main feature[] */
+    /* Combat I will do the last main feature[-] */
     public void west() {
+        // Forest
+        System.out.println("\n----------------------------------------------------------------");
+        System.out.println("You are in the forest and you see goblin wearing Legendary Ring as an amulet.\n");
+        System.out.println("You encounter goblin. Goblin sees you and say.\nGoblin: You no take my loot!\n");
+
+        System.out.println("1. Attack the goblin.");
+        System.out.println("2. Run.");
+        choice = scan.nextInt();
+        if (choice == 1) {
+            fight();
+        } else if (choice == 2) {
+            crossRoad();
+        } else {
+            west();
+        }
     }
 
-    /* This method is connected to West path method [] */
+    // -------------------------------------
+    public void fight() {
+        System.out.println("\n-----------------------");
+        System.out.println("You encounter a goblin!\n");
+        System.out.println("1. Fight.");
+        System.out.println("2. Run");
+        System.out.println("\n-----------------------");
+
+        choice = scanOne.nextInt();
+        if (choice == 1) {
+            attack();
+        }
+        if (choice == 1) {
+            crossRoad();
+        } else {
+            west();
+        }
+    }
+
+    public void monster() {
+    }
+    // -------------------------------------
+
+    /* This method is for combat feature [] */
     public void attack() {
+
+        // Creating Integer function with variable playerDamage
+        int playerDamage = 0;
+        // int weaponDamage;
+        if (playerWeapon.equals("Knife")) {
+            // weaponDamage = 5;
+            playerDamage = new java.util.Random().nextInt(5);
+        }
+        if (playerWeapon.equals("Excalibur")) {
+            // weaponDamage = 8;
+            playerDamage = new java.util.Random().nextInt(8);
+        }
+        // --------------------
+
+        // This function will choose random number between 0 - 9
+        // playerDamage = new java.util.Random().nextInt();
+        System.out.println("You attacked the goblin and deal " + playerDamage + " amount of damage!");
+
+        goblinHP = goblinHP - playerDamage;
+        System.out.println("Goblin HP: " + goblinHP);
+        if (goblinHP < 1) {
+            win();
+        }
+        if (goblinHP > 0) {
+            int monsterDamage = 0;
+            // --------------------
+            monsterDamage = new java.util.Random().nextInt(4);
+            // monsterDamage = new java.util.Random().nextInt(monsterDamage);
+            System.out.println("Goblin attacked you, and deal " + monsterDamage + " amount of damage!");
+            // --------------------
+            playerHP = playerHP - monsterDamage;
+
+            System.out.println("Player HP: " + playerHP);
+
+            if (playerHP < 1) {
+                dead();
+            } else if (playerHP > 0) {
+                fight();
+            }
+        }
+
+    }
+
+    public void dead() {
+        System.out.println("\n-----------------------");
+        System.out.println("\nGAME OVER!");
+        System.out.println("\n-----------------------");
+        playerInfo();
+    }
+
+    public void win() {
+        System.out.println("");
+        System.out.println(
+                "You defeated the goblin! and now you get Legendary Ring from goblin,\ntime to back to the Castle.");
+
+        end();
     }
 
     /*
      * Cave that have powerful sword
      * Challange is player need to answer the question if he does he will get a
-     * powerful sword;
+     * powerful sword [-]
      */
     public void east() {
+        // Cave
         System.out.println("You are in the mysteries cave, you see sword in the stone.\n");
         System.out.println(
-                "You touched the sword, want to pull sword out from the stone, but you hear noise that come from sword. Ah an adventure. I have one challange for you, answer me this and you will be able to pull the sword and wield this Legendary Sword. If you dont answer correctly you wont be able to pull the sword making you unworthy.");
+                "You touched the sword, want to pull sword out from the stone, but you hear noise that come from sword. Ah an adventure. I have one challange for you, answer me this and you will be able to pull the sword and wield this Legendary Sword. If you don't answer correctly you wont be able to pull the sword making you unworthy.");
         System.out.println("");
         System.out.println("1. Accept the challange.");
         System.out.println("2. Leave.");
         choice = scan.nextInt();
 
+        if (choice == 1) {
+            System.out.println(
+                    "Ha, good, good!! So you like some challange. But be wary adventurer, accepting this challange if you fail you may never come to this place again!");
+            challange();
+            System.out.println("\n----------------------------------------------------------------");
+        } else if (choice == 2) {
+            crossRoad();
+        }
+    }
+
+    public void challange() {
+        System.out.println(
+                "I have a hilt, but no door.\nI have a blade, but cannot soar.\nDragons fear me\nknights hold me dear.\nSpeak my punny name, and I shall appear.\nWhat am I?\n");
+        String correctAnswer = "Excalibur";
+
+        // Scanner scanOne = new Scanner(System.in);
+        String answer = scanOne.nextLine();
+        if (answer.equalsIgnoreCase(correctAnswer)) {
+            playerWeapon = "Excalibur";
+            System.out.println("\n----------------------------------------------------------------");
+            System.out.println("Congratulations! You are worthy to wield " + playerWeapon);
+            System.out.println("\n----------------------------------------");
+            System.out.println("You may leave the cave now!");
+            System.out.println("(Press any button to continue to leave a cave)");
+            choice = scan.nextInt();
+            if (choice == 1) {
+                crossRoad();
+            } else {
+                crossRoad();
+            }
+        } else {
+            System.out.println("You have failed!");
+        }
     }
 
     public void South() {
     }
 
-    public void castle() {
-    }
+    public void end() {
+        System.out.println("Good job adventurer for bringing me back my powerful ring!");
 
-    public void dead() {
+        System.out.println("\n----------------------------------------------------------------");
+        System.out.println("You defeated the game congratulations!");
+        System.out.println("The End");
+        System.out.println("\n----------------------------------------------------------------");
     }
 
 }
